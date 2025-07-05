7d2f493b01c9f06920f675d3ba248a712e98d449
linwanglue <346535692@qq.com>
2025-07-05 15:16:20 +0800

    [feat] code-generate

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.douniu"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // MyBatis 核心依赖
+    implementation("org.mybatis:mybatis:3.5.11")
+    // MyBatis Generator 依赖
+    implementation("org.mybatis.generator:mybatis-generator-core:1.4.1")
+    // 数据库驱动，这里以 MySQL 为例，根据实际情况替换
+    implementation("mysql:mysql-connector-java:8.0.28")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/douniu/codegenerate/CodeGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.douniu.codegenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGenerateApplication.class, args);
+    }
+
+}
